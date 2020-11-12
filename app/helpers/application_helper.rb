192e849dcb9f9757cf6b3dc9ad77f143d4257b0a
vikitorfg <victorgarzel@hotmail.com>
2020-11-12 16:22:25 -0300

    added bootstrap, refactored navbar, added navbar style and active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    def login_helper
+        nav_string = ''
+        if user_signed_in?
+            nav_string << "<li class='nav-item'>" + (link_to 'logout', destroy_user_session_path, method: :delete, class: "nav-link") + "</li> " + "<li class='nav-item'>" + (link_to 'account', edit_user_registration_path, class: "nav-link #{is_active? edit_user_registration_path}") + "</li>"
+            nav_string.html_safe
+        else 
+            nav_string << "<li class='nav-item'>" + (link_to 'login', new_user_session_path, class: "nav-link #{is_active? new_user_session_path}") + "</li> " + "<li class='nav-item'>" + (link_to 'register', new_user_registration_path, class: "nav-link #{is_active? new_user_registration_path}") + "</li>"
+            nav_string.html_safe
+        end
+    end
+
+    def is_active?(path)
+        'active' if current_page? path
+    end
+
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,14 @@
 const { environment } = require("@rails/webpacker");
-
 const webpack = require("webpack");
+
 environment.plugins.prepend(
   "Provide",
   new webpack.ProvidePlugin({
-    $: "jquery/src/jquery",
-    jQuery: "jquery/src/jquery",
+    $: "jquery",
+    jQuery: "jquery",
+    jquery: "jquery",
+    "window.jQuery": "jquery",
+    Popper: ["popper.js", "default"],
   })
 );
 
