ba047492d177274ac0f8d35c7eab2b380959273d
vikitorfg <victorgarzel@hotmail.com>
2020-11-13 14:09:08 -0300

    minor route fix, contacts-index design

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root to: "contacts#index"
 end
